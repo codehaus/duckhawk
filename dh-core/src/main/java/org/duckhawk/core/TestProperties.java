a95dd8a4724e8986e7c7a982cda637766b195977
aaime <aaime@3fb3efba-a649-0410-ba43-c1752987c092>
2008-04-22 14:11:10 +0000

    Javadoc fix

--- dh-core/src/main/java/org/duckhawk/core/TestProperties.java
+++ dh-core/src/main/java/org/duckhawk/core/TestProperties.java
@@ -3,10 +3,10 @@ package org.duckhawk.core;
 import java.util.Map;
 
 /**
- * Empty interface for test properties. A test properties is just a Map from
- * string keys to Object values. The test executor and the listener can use it
- * to share extra information that is not concerned directly with conformance
- * test results (pass yes/no) nor with performance test results (timings).
+ * A test properties is just a Map from string keys to Object values. <br>
+ * The test executor and the listener can use it to share extra information that
+ * is not concerned directly with conformance test results (pass yes/no) nor
+ * with performance test results (timings).
  * 
  * @author Andrea Aime (TOPP)
  * 
