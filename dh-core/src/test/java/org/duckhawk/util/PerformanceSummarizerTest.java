b4fbe52bd6c581bbe01496cab18fb33b19b71af1
aaime <aaime@3fb3efba-a649-0410-ba43-c1752987c092>
2008-04-22 13:50:11 +0000

    Added testing for the perf summarizer

---
+++ dh-core/src/test/java/org/duckhawk/util/PerformanceSummarizerTest.java
@@ -0,0 +1,59 @@
+package org.duckhawk.util;
+
+import junit.framework.TestCase;
+
+public class PerformanceSummarizerTest extends TestCase {
+    private PerformanceSummarizer summarizer;
+
+    @Override
+    protected void setUp() throws Exception {
+        summarizer = new PerformanceSummarizer();
+    }
+
+    public void testNoTimes() {
+        new SummarizerTestScaffolding(new double[0], summarizer,
+                Double.MAX_VALUE, Double.MIN_VALUE, Double.NaN, Double.NaN, 0.0)
+                .runTest();
+    }
+
+    public void testOneTime() {
+        new SummarizerTestScaffolding(new double[] { 152.0 }, summarizer,
+                152.0, 152.0, 152.0, 152.0, 152.0).runTest();
+    }
+
+    public void testTwoTimes() {
+        new SummarizerTestScaffolding(new double[] { 1, 3 }, summarizer, 1.0,
+                3.0, 2.0, 2.0, 4.0).runTest();
+    }
+
+    public void testThreeTimes() {
+        new SummarizerTestScaffolding(new double[] { 1, 6, 4 }, summarizer,
+                1.0, 6.0, 11.0 / 3.0, 4.0, 11.0).runTest();
+    }
+
+    /**
+     * Checks summarizer still works even when fed with a wrong expected size
+     */
+    public void testExpandArray() {
+        new SummarizerTestScaffolding(new double[] { 1, 6, 4 }, summarizer,
+                1.0, 6.0, 11.0 / 3.0, 4.0, 11.0, 1).runTest();
+    }
+
+    /**
+     * Checks summarizer still works even when fed with a wrong expected size
+     * (zero, in this case)
+     */
+    public void testExpandZeroSize() {
+        new SummarizerTestScaffolding(new double[] { 1, 6, 4 }, summarizer,
+                1.0, 6.0, 11.0 / 3.0, 4.0, 11.0, 0).runTest();
+    }
+    
+    /**
+     * Checks summarizer still works even when fed with a wrong expected size
+     * (negative, in this case)
+     */
+    public void testExpandNegativeSize() {
+        new SummarizerTestScaffolding(new double[] { 1, 6, 4 }, summarizer,
+                1.0, 6.0, 11.0 / 3.0, 4.0, 11.0, -10).runTest();
+    }
+}
---
+++ dh-core/src/test/java/org/duckhawk/util/SummarizerTestScaffolding.java
@@ -0,0 +1,105 @@
+package org.duckhawk.util;
+
+import junit.framework.Assert;
+
+import org.duckhawk.core.TestExecutor;
+import org.duckhawk.core.TestMetadata;
+import org.duckhawk.core.TestProperties;
+import org.duckhawk.core.TestPropertiesImpl;
+import org.easymock.EasyMock;
+
+public class SummarizerTestScaffolding {
+    double[] times;
+
+    private PerformanceSummarizer summarizer;
+
+    private TestMetadata metadata;
+
+    private TestExecutor executor;
+
+    private int callCount;
+
+    private double min;
+
+    private double max;
+
+    private double average;
+
+    private double median;
+
+    private double total;
+
+    private int expectedCallCount;
+    
+    public SummarizerTestScaffolding(double[] times,
+            PerformanceSummarizer summarizer, double min,
+            double max, double average, double median, double total) {
+        this(times, summarizer, min, max, average, median, total, times.length);
+    }
+
+    public SummarizerTestScaffolding(double[] times,
+            PerformanceSummarizer summarizer, double min,
+            double max, double average, double median, double total, int expectedCallCount) {
+        this.metadata = new TestMetadata("test", "whosGonnaTestTheTests", "0.1");
+        this.times = times;
+        this.summarizer = summarizer;
+        this.expectedCallCount = expectedCallCount;
+        this.callCount = times.length;
+        this.min = min;
+        this.max = max;
+        this.average = average;
+        this.median = median;
+        this.total = total;
+        this.executor = EasyMock.createNiceMock(TestExecutor.class);
+    }
+
+    public void runTest() throws AssertionError {
+        fillSummarizerManually(times);
+        performChecks(null);
+        TestProperties props = fillSummarizerEvents(times);
+        performChecks(props);
+    }
+
+    protected void performChecks(TestProperties properties) throws AssertionError {
+        Assert.assertEquals(callCount, summarizer.getCallCount());
+        Assert.assertEquals(min, summarizer.getMin());
+        Assert.assertEquals(max, summarizer.getMax());
+        Assert.assertEquals(average, summarizer.getAverage());
+        Assert.assertEquals(median, summarizer.getMedian());
+        Assert.assertEquals(total, summarizer.getTotal());
+
+        if (properties != null) {
+            Assert.assertEquals(Integer.valueOf(callCount), properties
+                    .get(TestExecutor.KEY_CALL_COUNT));
+            Assert.assertEquals(Double.valueOf(min), properties
+                    .get(TestExecutor.KEY_MIN_TIME));
+            Assert.assertEquals(Double.valueOf(max), properties
+                    .get(TestExecutor.KEY_MAX_TIME));
+            Assert.assertEquals(Double.valueOf(average), properties
+                    .get(TestExecutor.KEY_AVG_TIME));
+            Assert.assertEquals(Double.valueOf(median), properties
+                    .get(TestExecutor.KEY_MED_TIME));
+            Assert.assertEquals(Double.valueOf(total), properties
+                    .get(TestExecutor.KEY_TOTAL_TIME));
+        }
+    }
+
+    protected void fillSummarizerManually(double[] times) {
+        summarizer.start(expectedCallCount);
+        for (double time : times) {
+            summarizer.accumulate(time);
+        }
+        summarizer.done();
+    }
+
+    protected TestProperties fillSummarizerEvents(double[] times) {
+        TestProperties testProperties = new TestPropertiesImpl();
+        summarizer.testRunStarting(metadata, testProperties, expectedCallCount);
+        for (double time : times) {
+            summarizer.testCallExecuted(executor, metadata,
+                    new TestPropertiesImpl(), time, null);
+        }
+        summarizer.testRunCompleted(metadata, testProperties);
+        return testProperties;
+    }
+}
