ebfc8daf34cf57464a85d06a2c1ae163d387bd01
aaime <aaime@3fb3efba-a649-0410-ba43-c1752987c092>
2008-04-27 20:45:44 +0000

    Completed event to model converter

---
+++ dh-model/src/test/java/AbstractModelListenerTest.java
@@ -0,0 +1,63 @@
+import junit.framework.TestCase;
+
+import org.duckhawk.core.TestMetadata;
+import org.duckhawk.core.TestProperties;
+import org.duckhawk.core.TestPropertiesImpl;
+import org.duckhawk.core.TestType;
+import org.duckhawk.report.listener.AbstractModelListener;
+import org.duckhawk.report.model.TestCallDetail;
+import org.duckhawk.report.model.TestResult;
+
+public class AbstractModelListenerTest extends TestCase {
+
+    public TestCallDetail detail;
+
+    public TestResult endResult;
+
+    public TestResult startResult;
+
+    private TestModelLister listener;
+
+    private TestMetadata metadata;
+
+    private class TestModelLister extends AbstractModelListener {
+
+        @Override
+        protected void handleDetail(TestCallDetail detail) throws Exception {
+            AbstractModelListenerTest.this.detail = detail;
+
+        }
+
+        @Override
+        protected void testEnded(TestResult result) throws Exception {
+            AbstractModelListenerTest.this.endResult = result;
+
+        }
+
+        @Override
+        protected void testStarting(TestResult result) throws Exception {
+            AbstractModelListenerTest.this.startResult = result;
+        }
+    }
+    
+    @Override
+    protected void setUp() throws Exception {
+        listener = new TestModelLister();
+        metadata = new TestMetadata("ThisIsTheTest", "Product", "VapourWareEdition");
+        
+    }
+    
+    public void testStart() {
+        TestProperties props = new TestPropertiesImpl();
+        props.put("totalTime", new Double(2.3));
+        listener.testRunStarting(metadata, props, 20);
+        assertNotNull(startResult);
+        assertSame(startResult.getTestRun().getProductVersion().getProduct(), startResult.getTest().getProduct());
+        assertEquals(metadata.getProductId(), startResult.getTest().getProduct().getName());
+        assertEquals(metadata.getProductVersion(), startResult.getTestRun().getProductVersion().getVersion());
+        assertEquals(metadata.getTestId(), startResult.getTest().getName());
+        assertEquals(TestType.undetermined, startResult.getTest().getType());
+        assertEquals(1, startResult.getTestProperties().size());
+        assertEquals("2.3", startResult.getTestProperties().get("totalTime"));
+    }
+}
