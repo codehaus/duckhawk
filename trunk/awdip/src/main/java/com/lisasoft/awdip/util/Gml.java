0fae52fea2e9ea9eded1ce86ecfc011131eadd1f
vmische <vmische@3fb3efba-a649-0410-ba43-c1752987c092>
2008-05-01 01:28:00 +0000

    Requests had accidently a maxFeatures limit.

--- trunk/awdip/src/main/java/com/lisasoft/awdip/util/Gml.java
+++ trunk/awdip/src/main/java/com/lisasoft/awdip/util/Gml.java
@@ -4,7 +4,7 @@ public class Gml {
     
     public static String createAndFilterRequest(String typeName, String... filters) {
         StringBuffer request = new StringBuffer();
-        request.append("<wfs:GetFeature service=\"WFS\" version=\"1.1.0\" xmlns:ogc=\"http://www.opengis.net/ogc\" xmlns:om=\"http://www.opengis.net/om/1.0\" xmlns:wfs=\"http://www.opengis.net/wfs\" xmlns:sa=\"http://www.opengis.net/sampling/1.0\" xmlns:aw=\"http://www.water.gov.au/awdip\"  xmlns:ows=\"http://www.opengis.net/ows\" xmlns:gml=\"http://www.opengis.net/gml\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://www.opengis.net/wfs http://schemas.opengis.net/wfs/1.0.0/WFS-basic.xsd\" maxFeatures=\"500\">");
+        request.append("<wfs:GetFeature service=\"WFS\" version=\"1.1.0\" xmlns:ogc=\"http://www.opengis.net/ogc\" xmlns:om=\"http://www.opengis.net/om/1.0\" xmlns:wfs=\"http://www.opengis.net/wfs\" xmlns:sa=\"http://www.opengis.net/sampling/1.0\" xmlns:aw=\"http://www.water.gov.au/awdip\"  xmlns:ows=\"http://www.opengis.net/ows\" xmlns:gml=\"http://www.opengis.net/gml\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://www.opengis.net/wfs http://schemas.opengis.net/wfs/1.0.0/WFS-basic.xsd\">");
         request.append(" <wfs:Query typeName=\""+typeName+"\">");
         request.append("<ogc:Filter><ogc:And>");
 
