a7bbf4e5d20afd8f9e828e4aac8141a940f7b5fd
shansen <shansen@3fb3efba-a649-0410-ba43-c1752987c092>
2008-05-14 00:20:27 +0000

    A variation of the simples bbox test that gets the start and end bbox from a config file.

---
+++ awdip/src/main/java/com/lisasoft/awdip/util/InvalidConfigFileException.java
@@ -0,0 +1,30 @@
+package com.lisasoft.awdip.util;
+
+public class InvalidConfigFileException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -9146834224962646266L;
+
+	public InvalidConfigFileException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidConfigFileException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidConfigFileException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidConfigFileException(Throwable arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+}
