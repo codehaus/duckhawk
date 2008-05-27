0d8cb5047a954d13d8e08eb6c7098e9a2ad12801
aaime <aaime@3fb3efba-a649-0410-ba43-c1752987c092>
2008-05-27 10:42:15 +0000

    DH-18, Allow exception stack traces to be included in XML output when a verbose flag is raised

--- trunk/dh-core/src/main/java/org/duckhawk/util/ExceptionConverter.java
+++ trunk/dh-core/src/main/java/org/duckhawk/util/ExceptionConverter.java
@@ -1,5 +1,7 @@
 package org.duckhawk.util;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 /**
  * Simple utility class that does convert an exception into a string
@@ -10,6 +12,26 @@ package org.duckhawk.util;
  * 
  */
 public class ExceptionConverter {
+
+    protected boolean verbose;
+
+    /**
+     * A verbose exception converter will dump full stack traces, whilst a non
+     * verbose one will just dump the chain of error messages
+     * 
+     * @return
+     */
+    public boolean isVerbose() {
+        return verbose;
+    }
+
+    /**
+     * Sets/resets verbose mode
+     * @param verbose
+     */
+    public void setVerbose(boolean verbose) {
+        this.verbose = verbose;
+    }
 
     /**
      * Dumps an exception message along all its causes messages into a string
@@ -33,31 +55,39 @@ public class ExceptionConverter {
      *                check against self looping exception causes)
      */
     public String convert(Throwable t, int maxMessageCount) {
-        StringBuffer result = new StringBuffer();
-        result.append("[");
-        Throwable ex = t;
-        int count = 0;
-        do {
-            // append the message if not empty
-            count++;
-            final String message = ex.getMessage();
-            if (!"".equals(message))
-                result.append(message);
+        if (verbose) {
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            PrintStream ps = new PrintStream(bos);
+            t.printStackTrace(ps);
+            ps.flush();
+            return bos.toString();
+        } else {
+            StringBuffer result = new StringBuffer();
+            result.append("[");
+            Throwable ex = t;
+            int count = 0;
+            do {
+                // append the message if not empty
+                count++;
+                final String message = ex.getMessage();
+                if (!"".equals(message))
+                    result.append(message);
 
-            // grab the cause and make sure we do not enter into an infinite
-            // loop
-            // by adding a self loop failsafe and also a count to make sure
-            // 
-            Throwable cause = ex.getCause();
-            if (ex == cause || cause == null || count >= maxMessageCount) {
-                break;
-            } else {
-                result.append(", ");
-                ex = cause;
-            }
-        } while (true);
-        result.append("]");
-        return result.toString();
+                // grab the cause and make sure we do not enter into an infinite
+                // loop by adding a self loop failsafe and also a count to make
+                // sure
+                // 
+                Throwable cause = ex.getCause();
+                if (ex == cause || cause == null || count >= maxMessageCount) {
+                    break;
+                } else {
+                    result.append(", ");
+                    ex = cause;
+                }
+            } while (true);
+            result.append("]");
+            return result.toString();
+        }
     }
 
 }
