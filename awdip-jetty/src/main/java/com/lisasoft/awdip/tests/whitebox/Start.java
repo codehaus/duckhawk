656766d3c788e310ecf1203f24b365c0dcdc9ca9
camerons <camerons@3fb3efba-a649-0410-ba43-c1752987c092>
2008-08-10 11:40:41 +0000

    DH-27: Removed GPL license so that we can change to LGPL (with permission of author Andrea)

--- awdip-jetty/src/main/java/com/lisasoft/awdip/tests/whitebox/Start.java
+++ awdip-jetty/src/main/java/com/lisasoft/awdip/tests/whitebox/Start.java
@@ -1,7 +1,3 @@
-/* Copyright (c) 2001 - 2007 TOPP - www.openplans.org. All rights reserved.
- * This code is licensed under the GPL 2.0 license, availible at the root
- * application directory.
- */
 package com.lisasoft.awdip.tests.whitebox;
 
 import org.mortbay.jetty.Connector;
