ab5b7f91df2b8344202074d45b55b82addb67f9e
camerons <camerons@3fb3efba-a649-0410-ba43-c1752987c092>
2008-08-10 11:55:47 +0000

    DH-17 Added LGPL license headers

--- trunk/dh-core/src/test/java/org/duckhawk/util/ConformanceSummarizerTestScaffolding.java
+++ trunk/dh-core/src/test/java/org/duckhawk/util/ConformanceSummarizerTestScaffolding.java
@@ -1,3 +1,23 @@
+/*
+ *    DuckHawk provides a Performance Testing framework for load
+ *    testing applications and web services in an automated and
+ *    continuous fashion.
+ * 
+ *    http://docs.codehaus.org/display/DH/Home
+ * 
+ *    Copyright (C) 2008 TOPP - http://www.openplans.org.
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+
 package org.duckhawk.util;
 
 import java.util.Set;
